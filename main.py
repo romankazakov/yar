a7706798696823e2b227f6a571328ef0c8a80c96
romankazakov <romanya@gmail.com>
2014-09-30 10:35:05 +0400

    Main Page proccesor

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ class YarSpider(Spider):
     def prepare(self):
         self.result_file = csv.writer(open('result.txt', 'w')
     
+    def task_initial(self, grab, task):
+	css_path = "html.i-ua_js_yes body.b-pagea div.b-wrapper div.b-layout div.b-serp-wrapper table.b-layout__table tbody tr td.b-layout__table-left div.b-pager div.b-pager__pages a.b-pager__page[href]"
+	for elem in grab.css_list(css_path):
+	    yield Task('yarpage', url=elem.get('href'))
+
+    def task_yarpage(self,grab,task):
+	css_path = "html.i-ua_js_yes body.b-pagea div.b-wrapper div.b-layout div.b-serp-wrapper table.b-layout__table tbody tr td.b-layout__table-left div.b-serp-list div.b-serp-item"
+	for elem in grab.css_list(css_list):
+	    
+	
+
+    
     
